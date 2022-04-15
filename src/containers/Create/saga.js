b7a15a0656fcfe00adbb38016660fcbac0f8fca7
sanju-bisht <sandeep.bisht@giksindia.com>
2022-04-15 14:56:28 +0530

    updateAPI

--- src/containers/Create/saga.js
+++ src/containers/Create/saga.js
@@ -6,6 +6,7 @@ import { GetHeaders } from "../../utils";
 export function* saveRegisterUserData(action) {
   let url = "http://144.91.110.221:4801/api/saveregistreduser";
   // let url = "http://localhost:4801/api/saveregistreduser";
+  
   try {
     const response = yield call(axios.post, url, action.payload, GetHeaders());
     yield put({
@@ -20,7 +21,26 @@ export function* saveRegisterUserData(action) {
   }
 }
 
+export function* updateRegisteredUser(action) {
+  console.log("I am header",GetHeaders())
+  let url = `http://localhost:4801/api/updateregisteruserinfo/${action.id}`;
+  // let url = "http://localhost:4801/api/saveregistreduser";
+  try {
+    const response = yield call(axios.put, url, action.payload, GetHeaders());
+    yield put({
+      type: CONSTANTS.UPDATE_REGISTERED_USER_SUCCESS,
+      response: response.data,
+    });
+  } catch (error) {
+    yield put({
+      type: CONSTANTS.UPDATE_REGISTERED_USER_FAILURE,
+      error: error.response.data,
+    });
+  }
+}
+
 export function* getLoggedInUser(action) {
+  
   let url = `http://localhost:4801/api/getsaveregistreduserinfo/${action.id}`;
   try {
     const response = yield call(axios.get, url, GetHeaders());
@@ -40,4 +60,5 @@ export function* getLoggedInUser(action) {
 export default function* CreateSaga() {
   yield takeLatest(CONSTANTS.SAVE_REGISTER_USER_DATA, saveRegisterUserData);
   yield takeLatest(CONSTANTS.GET_LOGGEDID_USER, getLoggedInUser);
+  yield takeLatest(CONSTANTS.UPDATE_REGISTERED_USER, updateRegisteredUser);
 }
