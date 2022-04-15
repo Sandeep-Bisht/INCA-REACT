33a33e5db61d660329560a716e4bb12f8471a014
RohitDeshwal <rohitdeshwal418@gmail.com>
2022-04-15 18:11:35 +0530

    banner text correction

---
+++ src/containers/Dashboard/constant.js
@@ -0,0 +1,3 @@
+export const GET_COUNTER_USER = "app/GetCounterPage/GET_COUNTER_USER";
+export const GET_SPONSOR_USER_SUCCESS = "app/GetCounterPage/GET_COUNTER_USER_SUCCESS";
+export const GET_SPONSOR_USER_FAILURE = "app/GetCounterPage/GET_COUNTER_USER_FAILURE";
