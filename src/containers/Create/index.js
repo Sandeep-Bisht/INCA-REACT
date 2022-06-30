fdbc5f1f39cbbafa69d159ca76a3df142dc24367
Juleshpun <julesh.pun@giksindia.com>
2022-06-30 13:21:38 +0530

    accompany-ui

--- src/containers/Create/index.js
+++ src/containers/Create/index.js
@@ -9,6 +9,7 @@ import jwt_decode from "jwt-decode";
 import { useDispatch, useSelector } from "react-redux";
 import { countries } from "../../utils";
 import * as ACTIONS from "./action";
+import {BsFillTrashFill} from "react-icons/bs";
 
 const obj = {
   name: "",
@@ -719,9 +720,9 @@ const CreateForm = (props) => {
                       disabled={isDisabled}
                       id="conferenceMode"
                     >
-                      <option defaultValue hidden>
+                      {/* <option defaultValue hidden>
                         Please Select The Mode
-                      </option>
+                      </option> */}
                       {/* <option value="online">Online</option> */}
                       <option value="offline">Physical</option>
                     </select>
@@ -765,7 +766,7 @@ const CreateForm = (props) => {
 
               <div className="col-md-4">
                 <div className="row">
-                  <div className="col-md-12 mb-3">
+                  <div className="col-md-12 mb-4">
                     <label htmlFor="SelectWish" className="form-label asterisk">
                       I wish to participate in the conference for
                     </label>
@@ -797,15 +798,69 @@ const CreateForm = (props) => {
                       <p className="text-danger">{isError.participationType}</p>
                     )}
                   </div>
-                  {/* <div className="col-md-12 pt-5">
+                  <div className="col-md-12 mt-2">
+              <div>
+                <p className="form-label">Delegate :</p>
+              </div>
                     <div className="radio-button-box d-flex">
-                   <label for='foreigner'>foreigner</label>
-                    <input type='radio' id="foreigner" name="radio-btn"/>
-                  <label for='Indian'>Indian</label>
-                    <input type='radio' id="Indian" name="radio-btn"/>
+                   <label className="pe-2" for='foreigner'>Foreigner</label>
+                    <input type='radio' id="foreigner" className="w-auto" name="radio-btn"/>
+                  <label className="ps-4 pe-1" for='Indian'>Indian</label>
+                    <input type='radio' id="Indian" className="w-auto" name="radio-btn"/>
                     </div>
-                  </div> */}
-                  <div className="col-md-12">
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="InputTitle" className="form-label">
+                  Title of the paper/poster
+                </label>
+                <textarea
+                  id="title"
+                  value={userInformation && userInformation.title}
+                  disabled={isDisabled}
+                  onChange={(e) => userInformationOnchangeHandler(e)}
+                  className={
+                    isError && isError.title.length > 0
+                      ? "is-invalid form-control"
+                      : "form-control"
+                  }
+                ></textarea>                
+              </div>
+            </div>  
+
+            <div className="row">
+            
+              <div className="col-md-12 mb-4">
+                <div className="accompany-box d-flex">
+                  <div className="accompany-box-1 pt-1">
+                <h6 className="form-label m-0">Accompanying Person :</h6>
+                </div>
+                <div className="accompany-box-2 d-flex">
+                  <label className="ps-4 pe-1" for='yes'>Yes</label>
+                <input type='radio' id="yes" name='check'/>
+                <label className="ps-4 pe-1" for='no'>No</label>
+                <input type='radio' id="no" name='check'/>
+                </div>
+              </div>
+              <div className="add-button pt-2">
+                  <button className="create-btn" type="button">ADD</button>
+                </div>
+                <div className="exhibitor-relation d-flex mt-3">
+                  <div className="relation-box-1">
+                    <label className="form-label" for='relation-name'>Full Name</label>
+                  <input className="form-control" type='text' id='relation-name'/>
+                  </div>
+                  <div className="ms-2 relation-box-2">
+                  <label className="form-label" for='relation-type'>Relation</label>
+                  <input className="form-control" type='text' id='relation-type'/>
+                  </div>
+                  <div className="relation-delete-box ps-3">
+                  <BsFillTrashFill/>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
                     {isDisabled && (
                       <>
                         <label htmlFor="InputFee" className="form-label">
@@ -833,41 +888,7 @@ const CreateForm = (props) => {
                       </>
                     )}
                   </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <label htmlFor="InputTitle" className="form-label">
-                  Title of the paper/poster
-                </label>
-                <textarea
-                  id="title"
-                  value={userInformation && userInformation.title}
-                  disabled={isDisabled}
-                  onChange={(e) => userInformationOnchangeHandler(e)}
-                  className={
-                    isError && isError.title.length > 0
-                      ? "is-invalid form-control"
-                      : "form-control"
-                  }
-                ></textarea>                
-              </div>
-            </div>  
-
-            {/* <div className="row">
-              <div className="col-md-12">
-                <div className="accompany-box">
-                  <div className="accompany-box-1">
-                <h4>Accompanying Person</h4>
-                </div>
-                <div className="accompany-box-2">
-                  <label for='yes'>Yes</label>
-                <input type='checkbox' id="yes" name=''/>
-                <label for='no'>No</label>
-                <input type='checkbox' id="no" name=''/>
-                </div>
-              </div>
-              </div>
-              </div>           */}
+              </div>          
 
             {message && <p className={`${message == "Your information saved successfully" ? "text-success" : "text-danger"}`} >{message}</p>}
 
