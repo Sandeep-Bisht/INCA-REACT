4d683fefc868868338bb6f1dc7f09aa13ec4dd41
abhishekrautela123 <abhishek.rautela@giksindaia.com>
2023-07-21 16:46:42 +0530

    home page changes

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -104,7 +104,7 @@ const HomePage = () => {
         <div className="homepage-video-section">
           <video
             className="w-100"
-            // src={homepageBackgroundVideo}
+             src={homepageBackgroundVideo}
             autoPlay
             muted
             loop
@@ -132,9 +132,7 @@ const HomePage = () => {
                   <p className="head-text">
                    Economy
                   </p>
-                  <p className="head-text-para">
-                    -- Enabling Earth to Talk --
-                  </p>
+                  
                   {/* <span className="banner-last">Event-2023</span> */}
                 </p>
                 <div className="banner-bottom"></div>
@@ -160,6 +158,20 @@ const HomePage = () => {
                     Full Paper Submission
                   </button> */}
                 </div>
+                <div className="banner-btn-box pt-2">
+                  <button
+                    className=" common-btn register-home-btn"
+                    onClick={() => navigation("/register")}
+                  >
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <a target="_blank" href={FinalSercular}>
+                   First Circular 
+                   </a>
+                  </button>
+                  </div>
               </div>
             </div>
           </div>
@@ -202,18 +214,18 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-          {/* <section className="abstract-date">
+           <section className="abstract-date">
   <div className="container-fluid">
   <div className="row">
             <div className="col-md-12">
               <div className="abstract-submission-date">
-            <marquee><p><b>"The Full paper Submission Date for 42nd INCA Conference is 20<sup>th</sup> September 2023"</b></p></marquee>
-            <marquee><p><b>"The Full paper Submission Date for 42nd INCA Conference is  (Extended) 10<sup>th</sup>October 2023"</b></p></marquee>
+            <marquee><p><b>"You can now start registration for 43<sup>rd</sup> INCA Conference 2023"</b></p></marquee>
+            {/* <marquee><p><b>"The Full paper Submission Date for 42nd INCA Conference is  (Extended) 10<sup>th</sup>October 2023"</b></p></marquee> */}
             </div>
             </div>
           </div>
   </div>
-</section> */}
+</section> 
         </div>
       </div>
 
@@ -261,28 +273,29 @@ const HomePage = () => {
       <section className="section-padding home-about pb-lg-0 without-carousel event-bg ">
         <div className="container">
           <div className="row py-4 mt-3">
-            <div className="col-lg-4">
+             <div className="col-lg-4">
               <div className="home-about-event-card">
                 <div className="left">
                   <span className="first common-yellow-bg">
                     <i className="fa-solid fa-location-dot"></i>
                   </span>
                 </div>
-                <div className="right">
+                <div className="right mt-3">
                   <h2 className="h2">Where is the Event</h2>
                   <p className="common-para">
                     <b>
                       {" "}
-                      Regional Remote Sensing Center-West National Remote
-                      Sensing Center (NRSC),ISRO ISRO Complex,Bypass Road
+                      <h5 className="right-text">Regional Remote Sensing Center-West</h5> National Remote
+                      Sensing Center NRSC/ISRO, ISRO Complex,Bypass Road
                       Sector 9,Kudi Bhagtasani Housing Board (KBHB)
                     </b>{" "}
                     <br />
                     <b>Jodhpur – 342005</b>
                   </p>
                 </div>
-              </div>
-            </div>
+              </div></div> 
+            
+
             <div className="col-lg-4">
               <div className="home-about-event-card">
                 <div className="left">
@@ -290,7 +303,7 @@ const HomePage = () => {
                     <i className="fa-solid fa-calendar-days"></i>
                   </span>
                 </div>
-                <div className="right">
+                <div className="right mt-3">
                   <h2 className="h2">When is the Event</h2>
                   <p className="common-para">
                     <b>06 November 2023</b> <br /> <b>07 November 2023</b>{" "}
@@ -306,11 +319,16 @@ const HomePage = () => {
                     <i className="fa-solid fa-calendar-days"></i>
                   </span>
                 </div>
-                <div className="right">
+                <div className="right mt-3">
                   <h2 className="h2">Schedule of the Event</h2>
                   <Link className="common-para pb-3" to="/importantdates">
                     <b>Click here to know details.</b>
-                  </Link>
+                   
+                  </Link><br/>
+                  <p className="common-para">
+                    <b>Last date for submission of abstract</b></p>
+                  <p className="common-para pb-3">
+                    <b>10/August/2023</b></p> 
                   {/* <p className="common-para">
                     Last date for submitting the abstracts
                   </p>
@@ -700,9 +718,9 @@ const HomePage = () => {
               showDots={false}
               responsive={responsive}
               // ssr={true}
-              // infinite={true}
-              autoPlay={false}
-              autoPlaySpeed={50000}
+              infinite={true}
+              autoPlay={true}
+              autoPlaySpeed={3000}
               // keyBoardControl={true}
               // transitionDuration={500}
             >
@@ -2398,7 +2416,7 @@ const HomePage = () => {
         </div>
       </section>
 
-      <section className="sponsors-area">
+      <section className="sponsors-area event-bg">
         <div className="container">
           <div className="row position-relative">
             <div className="col-md-7 mx-auto text-center">
